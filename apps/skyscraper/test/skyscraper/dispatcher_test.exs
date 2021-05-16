eb638400d0ac556e22cc40b3461f8a0465dc606e
dmitrykleymenov <dukefruitarian@gmail.com>
2021-05-16 10:05:08 +0700

    Add more tests for dispatcher

--- apps/skyscraper/test/skyscraper/dispatcher_test.exs
+++ apps/skyscraper/test/skyscraper/dispatcher_test.exs
@@ -2,6 +2,10 @@ defmodule Skyscraper.DispatcherTest do
   alias Skyscraper.Dispatcher
   use ExUnit.Case, async: true
 
+  setup do
+    %{dispatcher: Dispatcher.build(elevator_ids: [1, 2, 3], floors: 1..40 |> Enum.to_list())}
+  end
+
   test "builds a Dispatcher struct with the given params" do
     dispatcher = Dispatcher.build(elevator_ids: [1, 2], floors: [4, 5, 6])
     assert %Dispatcher{} = dispatcher
@@ -14,15 +18,11 @@ defmodule Skyscraper.DispatcherTest do
            |> Dispatcher.active_buttons() == [{5, :up}, {7, :down}]
   end
 
-  test "returns elevator ids" do
-    assert %Dispatcher{elevators: %{1 => nil, 2 => nil}} |> Dispatcher.elevator_ids() == [1, 2]
+  test "returns elevator ids", %{dispatcher: dispatcher} do
+    assert dispatcher |> Dispatcher.elevator_ids() == [1, 2, 3]
   end
 
   describe ".push_button/3" do
-    setup do
-      %{dispatcher: Dispatcher.build(elevator_ids: [1, 2, 3], floors: 1..40 |> Enum.to_list())}
-    end
-
     test "just adds request to query when nobody can handle it", %{dispatcher: dispatcher} do
       {instructions, dispatcher} =
         dispatcher |> Dispatcher.push_button({5, :down}, %{1 => nil, 2 => nil, 3 => nil})
@@ -53,10 +53,46 @@ defmodule Skyscraper.DispatcherTest do
   end
 
   describe ".set_time_to_destination/3" do
+    test "updates destination info for appropriate elevator", %{dispatcher: dispatcher} do
+      {instructions, dispatcher} =
+        dispatcher |> Dispatcher.set_time_to_destination(1, {{7, :up}, 4000})
+
+      assert instructions |> Enum.empty?()
+      assert dispatcher.elevators[1] == {{7, :up}, 4000}
+    end
+
+    test "updates destination info, send canceling instruction and clears destination info for less efficient elevator",
+         %{
+           dispatcher: dispatcher
+         } do
+      {instructions, dispatcher} =
+        dispatcher
+        |> Map.put(:elevators, %{1 => nil, 2 => {{7, :up}, 6000}, 3 => nil})
+        |> Dispatcher.set_time_to_destination(1, {{7, :up}, 4000})
+
+      assert instructions == [{:cancel_request, 2, {7, :up}}]
+      assert dispatcher.elevators[1] == {{7, :up}, 4000}
+      assert dispatcher.elevators[2] == nil
+    end
+  end
+
+  describe ".request_handled/3" do
+    test "clears destination info for the elevator and removes request from queue", %{
+      dispatcher: dispatcher
+    } do
+      dispatcher =
+        dispatcher
+        |> Map.put(:queue, [{7, :up}, {3, :down}])
+        |> Map.put(:elevators, %{1 => nil, 2 => nil, 3 => {{7, :up}, 1000}})
+        |> Dispatcher.request_handled(3, {7, :up})
+
+      assert dispatcher.elevators == %{1 => nil, 2 => nil, 3 => nil}
+      assert dispatcher |> Dispatcher.active_buttons() == [{3, :down}]
+    end
   end
 
   describe ".button_active?/2" do
-    test "truthy when button in queue" do
+    test "truthy when the button in queue" do
       assert %Dispatcher{queue: [{5, :up}]} |> Dispatcher.button_active?({5, :up})
     end
 
