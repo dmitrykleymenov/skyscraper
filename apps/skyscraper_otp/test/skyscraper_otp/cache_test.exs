4e50442c8c1d4287f84933392cfc245625a00891
dmitrykleymenov <dukefruitarian@gmail.com>
2021-05-27 18:48:06 +0300

    Add cache tests

--- apps/skyscraper_otp/test/skyscraper_otp/cache_test.exs
+++ apps/skyscraper_otp/test/skyscraper_otp/cache_test.exs
@@ -1,10 +1,56 @@
 defmodule SkyscraperOtp.CacheTest do
+  alias SkyscraperOtp.Cache
   use ExUnit.Case, async: true
 
   test "creates ets tables for elevators and dispatchers on start", %{test: name} do
-    {:ok, _} = start_supervised({SkyscraperOtp.Cache, name: name})
+    assert :ets.whereis(:"#{name}_elevators") == :undefined
+    assert :ets.whereis(:"#{name}_dispatchers") == :undefined
+
+    {:ok, _} = start_supervised({Cache, name: name})
 
     refute :ets.whereis(:"#{name}_elevators") == :undefined
     refute :ets.whereis(:"#{name}_dispatchers") == :undefined
+  end
+
+  test "adds dispatcher to cache", %{test: name} do
+    {:ok, cache} = start_supervised({Cache, name: name})
+    assert :ets.lookup(:"#{name}_dispatchers", "building_name") == []
+    Cache.update_dispatcher("building_name", "some_dispatcher", cache)
+    Process.sleep(1)
+
+    assert :ets.lookup(:"#{name}_dispatchers", "building_name") == [
+             {"building_name", "some_dispatcher"}
+           ]
+  end
+
+  test "gets dispatcher from cache", %{test: name} do
+    {:ok, cache} = start_supervised({Cache, name: name})
+
+    refute Cache.get_dispatcher("building_name", name)
+    Cache.update_dispatcher("building_name", "some_dispatcher", cache)
+    Process.sleep(1)
+
+    assert Cache.get_dispatcher("building_name", name) == "some_dispatcher"
+  end
+
+  test "adds elevator to cache", %{test: name} do
+    {:ok, cache} = start_supervised({Cache, name: name})
+    assert :ets.lookup(:"#{name}_elevators", {"building_name", "id"}) == []
+    Cache.update_elevator("building_name", "id", "some_elevator", cache)
+    Process.sleep(1)
+
+    assert :ets.lookup(:"#{name}_elevators", {"building_name", "id"}) == [
+             {{"building_name", "id"}, "some_elevator"}
+           ]
+  end
+
+  test "gets elevator from cache", %{test: name} do
+    {:ok, cache} = start_supervised({Cache, name: name})
+
+    refute Cache.get_elevator("building_name", "id", name)
+    Cache.update_elevator("building_name", "id", "some_elevator", cache)
+    Process.sleep(1)
+
+    assert Cache.get_elevator("building_name", "id", name) == "some_elevator"
   end
 end
